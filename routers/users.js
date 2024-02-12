7c85779ebe54a97170d5271e8876b6866a31eced
Raul Alvarez <raul.alva1926@gmail.com>
2024-02-11 21:13:40 -0600

    Vercel config 10

--- routers/users.js
+++ routers/users.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
     if (isValid) {
       uploadError = null;
     }
-    cb(uploadError, "./public/uploads");
+    cb(uploadError, "/tmp");
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname
